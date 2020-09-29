c1715d2e30c009d80f3f5ef57fb1ddc7208a2b4b
Edwin Fabian Vesga Escobar <edwfves@gmail.com>
2020-09-28 19:47:33 -0500

    ultimos cambios.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,28 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ErdlmNavBarComponent } from './presentation/erdlm-nav-bar/erdlm-nav-bar.component';
+import { AppRoutingModule } from './app-routing.module';
+import { LoginComponent } from './presentation/login/login.component';
+import { CommentBoxComponent } from './presentation/comment-box/comment-box.component';
+import { HomeComponent } from './presentation/home/home.component';
+import { ForoComponent } from './presentation/foro/foro.component';
+import { TemaComponent } from './presentation/tema/tema.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ErdlmNavBarComponent,
+    LoginComponent,
+    CommentBoxComponent,
+    HomeComponent,
+    ForoComponent,
+    TemaComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
